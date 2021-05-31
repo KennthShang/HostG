9f54509f7ebbdbd13a1a1bf9304bd2525df9e6b1
Kenneth Shang <kenneth.shang@foxmail.com>
2021-05-31 12:32:08 +0800

    update graph

--- run_KnowledgeGraph.py
+++ run_KnowledgeGraph.py
@@ -49,3 +49,73 @@ with open(phage_host_ntw) as file_in:
 ############################  Nodes construction ###############################
 ################################################################################
 
+# required contig2id: Dict type
+# required phage2id: Dict type
+# required id2contigF: List type
+# required id2phageF: List type
+node_feature = []
+for node in G.nodes():
+    # if query node
+    if node in contig2id.keys():
+        node_feature.append(id2contigF[contig2id[node]])
+    # if phage node
+    elif node in phage2id.keys():
+        node_feature.append(id2pahgeF[phage2id[node]])
+    # if bacteria node
+    else:
+        for neighbor in G.edges(nodes):
+            cnt = 0
+            tmpF = 0
+            # if neighbor is query node
+            if neighbor in contig2id.keys():
+                tmpF += id2contigF[contig2id[node]]
+            # if neighbor is phage node
+            elif neighbor in phage2id.keys():
+                tmpF += id2pahgeF[phage2id[node]]
+            cnt+=1
+        node_feature.append(tmpF/cnt)
+
+
+################################################################################
+############################  Label construction ###############################
+################################################################################
+
+
+# required Label.csv (label_df): DataFrame type
+# required taxa: String type
+node_label = []
+for node in G.nodes():
+    # if query node
+    if node in contig2id.keys():
+        neighbor_label = []
+        for _, neighbor in G.edges(node):
+            if neighbor not in contig2id.keys():
+                tmpL = label_df[label_df['accession'] == node][neighbor].values[0]
+                neighbor_label.append(tmpL)
+
+        if len(set(neighbor_label)) == 1:
+            node_label.append(neighbor_label[0])
+        else:
+            node_label.append(-1)
+
+    # if phage or host node
+    else:
+        tmpL = label_df[label_df['accession'] == node][taxa].values[0]
+        node_label.append(tmpL)
+
+
+# Convert string label to int label
+label_set = list(set(node_label))
+label2int = {tmpL:tmpid for tempL, tmpid in enumerate(label_set)}
+
+
+test_id = []
+node_int_label = []
+for label in node_label:
+    # labeled nodes
+    if label != -1:
+        node_int_label.append(label2int[label])
+    else:
+        node_int_label.append(-1)
+        test_id.append(idx)
+    idx+=1
